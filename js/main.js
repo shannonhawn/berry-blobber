04598c1e34d8d2c3f430080b03c5cc4c3acb1747
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-10 17:48:26 -0500

    put info from contact list generator and switched out item for berry

--- js/main.js
+++ js/main.js
@@ -33,9 +33,39 @@ var redCurrant = {
 // END DO NOT CHANGE ZONE
 // ##########################################################
 
-
 // Write your function here, name it `writeBerries`
 
+var writeBerries = function (berries) {
+  document.write('<h1>Berries</h1>');
+
+berries.forEach(function (berry) {
+    document.write('<h2>' + berry.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>E-mail address</dt>');
+    document.write('<dd><a href="mailto:' + item.email + '">' + item.email + '</a></dd>');
+    document.write('<dt>Phone number</dt>');
+    document.write('<dd><a href="tel:' + item.tel + '">' + item.tel + '</a></dd>');
+    document.write('<dt>Location</dt>');
+    document.write('<dd>Between ' + item.loc[0] + ' km & ' + item.loc[1] + ' km from the Sun.');
+    document.write('</dl>');
+    
+  });
+  };
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+}
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
